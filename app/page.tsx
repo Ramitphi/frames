f790601171d40b4df9c6d9b3968387cd46b9ce61
Ramitphi <18ucs144@lnmiit.ac.in>
2024-02-03 15:17:35 +0530

    commented frames metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,36 @@ import type { Metadata } from 'next';
 
 const NEXT_PUBLIC_URL = 'https://frames-beryl.vercel.app/';
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Click Me',
-    },
-  ],
-  image: `${NEXT_PUBLIC_URL}/park-1.png`,
-  input: {
-    text: 'Tell me a boat story',
-  },
-  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
-});
+// const frameMetadata = getFrameMetadata({
+//   buttons: [
+//     {
+//       label: 'Click Me',
+//     },
+//   ],
+//   image: `${NEXT_PUBLIC_URL}/park-1.png`,
+//   input: {
+//     text: 'Tell me a boat story',
+//   },
+//   post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+// });
 
-export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
-  description: 'LFG',
-  openGraph: {
-    title: 'zizzamia.xyz',
-    description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+// export const metadata: Metadata = {
+//   title: 'ramit.xyz',
+//   description: 'LFG',
+//   openGraph: {
+//     title: 'ramit',
+//     description: 'LFG',
+//     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+//   },
+//   other: {
+//     ...frameMetadata,
+//   },
+// };
 
 export default function Page() {
   return (
     <>
-      <h1>zizzamia.xyz</h1>
+      <h1>ramit</h1>
     </>
   );
 }
