6ca9641aef4cb33d6b081d11c7a3348129b0acf9
Ramitphi <18ucs144@lnmiit.ac.in>
2024-02-03 14:55:44 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+
+const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Click Me',
+    },
+  ],
+  image: `./park-1.png`,
+  input: {
+    text: 'Tell me a boat story',
+  },
+  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+});
+
+export const metadata: Metadata = {
+  title: 'zizzamia.xyz',
+  description: 'LFG',
+  openGraph: {
+    title: 'zizzamia.xyz',
+    description: 'LFG',
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>zizzamia.xyz</h1>
+    </>
+  );
+}
